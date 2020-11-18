94b2a1200518803a8a6a888be202c3e7c830ed30
malykdim <38568843+malykdim@users.noreply.github.com>
2020-11-18 23:36:01 +0100

    6-20-7-classVehicle.js

---
+++ 6-20-7-classVehicle.js
@@ -0,0 +1,24 @@
+function classVehicle(input) {
+    
+    class Vehicle {
+        constructor(type, model, parts, fuel, drive) {
+            this.type = type,
+            this.model = model,
+            this.parts = parts,
+            this.fuel = fuel;            
+        }
+        
+        drive(fuelLoss) {() => fuel -= fuelLoss};
+    } 
+    
+    
+}
+
+classVehicle(const Vehicle = result;
+
+    let parts = {engine: 6, power: 100};
+    let vehicle = new Vehicle('a', 'b', parts, 200);
+    vehicle.drive(100);
+    
+    expect(vehicle.fuel).to.equal(100);
+    expect(vehicle.parts.quality).to.equal(600););
