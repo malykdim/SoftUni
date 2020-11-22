6caa78ce0f7e6a4d971a726e5084d8eaecbd7c32
malykdim <38568843+malykdim@users.noreply.github.com>
2020-11-22 19:40:46 +0100

    initiating folder Basic / 1-6-11-bitcoinMining.js
    // Judge: 90/100

---
+++ Basic/1-6-11-bitcoinMining.js
@@ -0,0 +1,39 @@
+function bitcoinMining(shift){
+    const bitcoinPrice = 11949.16;
+    const goldPrice = 67.51;
+    let index = 0;
+    let digUp = 0;
+    let day = 0;
+    let money = 0;
+    let currentPurchase = 0;
+    let bitcoinsBought = 0;
+    let isfirstPurchase = false;
+    let dayOfFirstPurchase = 0;
+    while (day <= shift.length - 1) {
+        day++;
+        digUp = shift[index];
+        if (day % 3 === 0) {
+            digUp -= digUp * 0.3;
+        }
+        money += digUp * goldPrice;        
+        if (money >= bitcoinPrice) {            
+            if (bitcoinsBought === 0) {
+                isfirstPurchase = true;
+                if (isfirstPurchase) {
+                    let currentDay = day;
+                    dayOfFirstPurchase = currentDay;
+                }
+                isfirstPurchase = false;
+            }
+            currentPurchase = Math.floor(money / bitcoinPrice);
+            bitcoinsBought += currentPurchase;
+            money -= currentPurchase * bitcoinPrice;
+        }        
+        index++;
+    }
+    console.log(`Bought bitcoins: ${bitcoinsBought}`);  
+    if (bitcoinsBought > 1) {
+        console.log(`Day of the first purchased bitcoin: ${dayOfFirstPurchase}`);
+    }       
+    console.log(`Left money: ${money.toFixed(2)} lv.`);
+}
