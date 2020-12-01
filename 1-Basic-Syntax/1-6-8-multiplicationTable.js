14bac75daa1575fdf87095c0b3c24d5eed8870f6
Albena Kostova <38568843+malykdim@users.noreply.github.com>
2020-12-01 17:00:59 +0100

    6-9

---
+++ 1-Basic-Syntax/1-6-8-multiplicationTable.js
@@ -0,0 +1,14 @@
+/* function solve(num){
+    for (let i = 1; i <= 10; i++){
+        let result = i * num;
+        console.log(`${num} X ${i} = ${result}`);
+    }
+}
+solve(6);//100/100 */
+
+function solve(num){
+    for (let i = 1; i <= 10; i++){
+        console.log(`${num} X ${i} = ${i * num}`);
+    }
+}
+solve(6);//100/100
