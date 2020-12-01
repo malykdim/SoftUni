b3a3e8ae9496454c5e990111405fb1f1bd747be0
Albena Kostova <38568843+malykdim@users.noreply.github.com>
2020-12-01 16:51:10 +0100

    Delete 5-16-7-listOfProducts.js

--- 1-Basic-Syntax/5-16-7-listOfProducts.js
+++
@@ -1,8 +0,0 @@
-function listOfProducts(array) {
-    let sorted = array.sort();
-    for (let i = 0; i < array.length; i++) {
-        console.log(`${i+1}. ${sorted[i]}`);
-    }
-}
-// moe - 100/100
-listOfProducts(["Potatoes", "Tomatoes", "Onions", "Apples"]);
