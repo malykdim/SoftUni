b111c33b7ac66fa3e05b3618bb2fe9b4d3269bb5
Albena Kostova <albena.kostov@gmail.com>
2020-12-01 16:07:29 +0100

    ordering the mess

--- Basic/1-more-5-distanceBetweenPoints.js
+++
@@ -1,6 +0,0 @@
-function distanceBetweenPoints(x1, y1, x2, y2){
-    let result = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));    
-    console.log(Math.abs(result));
-}
-distanceBetweenPoints(2, 4, 5, 0); // 5
-distanceBetweenPoints(2.34, 15.66, -13.55, -2.9985); // 24.50778901186315
