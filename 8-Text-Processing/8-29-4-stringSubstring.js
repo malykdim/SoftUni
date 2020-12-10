c4bbdd4e5362a346e49aca62bdaa5439d4c503d9
Albena Kostova <38568843+malykdim@users.noreply.github.com>
2020-12-10 20:25:17 +0100

    8-29-4-stringSubstring.js

---
+++ 8-Text-Processing/8-29-4-stringSubstring.js
@@ -0,0 +1,13 @@
+function stringSubstring(word, str) {
+    str = str.toLowerCase().split(' ');
+    if (str.includes(word.toLowerCase())) {
+        console.log(word);        
+    } else {
+        console.log(`${word} not found!`);
+    }
+}
+
+stringSubstring('javascript',
+'JavaScript is the best programming language');
+stringSubstring('python',
+'JavaScript is the best programming language');
