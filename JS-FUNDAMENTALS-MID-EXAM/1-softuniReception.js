db8e922e01bde28d74f241c88c2f665292db04b8
Albena Kostova <38568843+malykdim@users.noreply.github.com>
2020-12-14 20:26:21 +0100

    Update 1-softuniReception.js

--- JS-FUNDAMENTALS-MID-EXAM/1-softuniReception.js
+++ JS-FUNDAMENTALS-MID-EXAM/1-softuniReception.js
@@ -3,20 +3,20 @@ function softuniReception(input) {
     let efficiency = eff1 + eff2 + eff3;
     
     let hour = 0;
-    while (students > 0) { // ako imame rabota, chakk togawa kazvame, che chasa e pochnal!
+    while (students > 0) { 
         hour++;
         
-        if (hour % 4 !== 0) { // ako chasa ne e 4-ti
+        if (hour % 4 !== 0) { 
             students -= efficiency;
-        } // v protiven sluchaj nishto ne pravim
+        } 
     }
     console.log(`Time needed: ${hour}h.`);
 }
 // 100/100
-softuniReception(['5', '6', '4', '20']); // 2
-softuniReception(['1', '2', '3', '45']); // 10
-softuniReception(['3', '2', '5', '40']); // 5
-softuniReception(['1', '1', '1', '0']); // 0
-softuniReception(['1', '1', '1', '10000']); // 4445
-softuniReception(['100', '100', '100', '0']); // 0
-softuniReception(['100', '100', '100', '10000']); // 45
+softuniReception(['5', '6', '4', '20']); 
+softuniReception(['1', '2', '3', '45']); 
+softuniReception(['3', '2', '5', '40']); 
+softuniReception(['1', '1', '1', '0']); 
+softuniReception(['1', '1', '1', '10000']); 
+softuniReception(['100', '100', '100', '0']); 
+softuniReception(['100', '100', '100', '10000']); 
