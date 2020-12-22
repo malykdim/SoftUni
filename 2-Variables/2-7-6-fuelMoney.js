2f70fded3729fa43060fe5877b77f0e4e5b6d5e2
Albena Kostova <38568843+malykdim@users.noreply.github.com>
2020-12-22 14:08:34 +0100

    Update 2-7-6-fuelMoney.js

--- 2-Variables/2-7-6-fuelMoney.js
+++ 2-Variables/2-7-6-fuelMoney.js
@@ -4,5 +4,6 @@ function fuelMoney(distance, passengers, price){
     let money = neededFuel * price;
     console.log(`Needed money for that trip is ${money}lv.`);
 }
+
 fuelMoney(260, 9, 2.49);
 fuelMoney(90, 14, 2.88);
