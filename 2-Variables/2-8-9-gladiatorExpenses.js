cdb392e310f34c5ba40e16f3ac775a98bd1dda58
Albena Kostova <38568843+malykdim@users.noreply.github.com>
2020-12-22 14:13:17 +0100

    Update 2-8-9-gladiatorExpenses.js

--- 2-Variables/2-8-9-gladiatorExpenses.js
+++ 2-Variables/2-8-9-gladiatorExpenses.js
@@ -18,8 +18,10 @@ function gladiatorExpenses(losses, helm, sword, shield, armor) {
             } 
         }        
     }
+    
     console.log(`Gladiator expenses: ${expenses.toFixed(2)} aureus`);
 }
+
 gladiatorExpenses(7, 2, 3, 4, 5);
 gladiatorExpenses(23, 12.50, 21.50, 40, 200);
 
