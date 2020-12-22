a32b93f6f914caa9c7b6aa081b7bbf5eab3f4673
Albena Kostova <38568843+malykdim@users.noreply.github.com>
2020-12-22 14:12:07 +0100

    Update 2-8-5-poundsToDollars.js

--- 2-Variables/2-8-5-poundsToDollars.js
+++ 2-Variables/2-8-5-poundsToDollars.js
@@ -2,9 +2,8 @@ function poundsToDollars(pounds) {
     const dollars = 1.31 * pounds;
     console.log(dollars.toFixed(3));
 }
+
 poundsToDollars(80);
 poundsToDollars(39);
-// 100/100
 
-// covert
-// fix to 3rd decimal point
+// 100/100
