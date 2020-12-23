0057b4fe5382172e130fbc38423f96d4e60ab69d
Albena Kostova <38568843+malykdim@users.noreply.github.com>
2020-12-23 22:04:43 +0100

    Update 3-10-4-reverseArrayOfStrings.js

--- 3-Arrays/3-10-4-reverseArrayOfStrings.js
+++ 3-Arrays/3-10-4-reverseArrayOfStrings.js
@@ -1,45 +1,33 @@
 
-/* function reverseStrings(input) {
+function reverseStrings(input) {
     for (let i = 0;i < input.length / 2; i++) {
-        let oldElement = input[i];
-        //console.log(oldElement);
-        let previousIndex = input.length - 1 - i;
-        //console.log(previousIndex);
-        input[i] = input[previousIndex];
-        //console.log(input[i]);
-        input[previousIndex] = oldElement;
-        //console.log(input[previousIndex]);
+        let oldElement = input[i];        
+        let previousIndex = input.length - 1 - i;        
+        input[i] = input[previousIndex];        
+        input[previousIndex] = oldElement;        
     }
+    
     console.log(input.join(' '));
 }
-reverseStrings(['a', 'b', 'c', 'd', 'e']); // e d c b a   
-// 100/100  */ 
 
-// reverseArrayOfStrings('abc', 'def', 'hig', 'klm', 'nop'); 
-// reverseArrayOfStrings('33', '123', '0', 'dd'); 
+reverseStrings(['a', 'b', 'c', 'd', 'e']); // e d c b a  
+reverseArrayOfStrings('abc', 'def', 'hig', 'klm', 'nop'); 
+reverseArrayOfStrings('33', '123', '0', 'dd'); 
 
-/* Victor */
-/* 
+// 100/100  
+ 
 function swap(arr) {
     let lastIndex = arr.length - 1;
-    // за всички еленети в масива:
     
-    for (let i = 0; i < arr.length / 2; i++) {
-        // - запазваме текущата стойност на елемент от масива в странична променлива
-        let temp = arr[i]; 
+    for (let i = 0; i < arr.length / 2; i++) {        
+        let temp = arr[i];        
         
-        // - намираме разстоянието -> [lastIndex - i]
-        // - определяме огледалния индекс -> arr[lastIndex - i]        
-        //  -- на мястото на текущия елемент записваме стойността на огледалния елемент 
-        arr[i] = arr[lastIndex - i];   
+        arr[i] = arr[lastIndex - i];           
         
-        // на мястото на финалния елемент слагаме стойността на временната променлива (разменяме)
         arr[lastIndex - i] = temp;
     }
-    
-    // отпечатваме elementite, разделени с интервал
+        
     console.log(arr.join(' '));
 }
-swap(['a', 'b', 'c', 'd', 'e']); // e d c b a
-// 100/100
- */
+
+// 100/100 /* Victor */
