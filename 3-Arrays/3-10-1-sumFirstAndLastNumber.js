7ab2abb572f3f3723fbfc72d1c62d08dcf11b91a
Albena Kostova <38568843+malykdim@users.noreply.github.com>
2020-12-23 21:58:54 +0100

    Update 3-10-1-sumFirstAndLastNumber.js

--- 3-Arrays/3-10-1-sumFirstAndLastNumber.js
+++ 3-Arrays/3-10-1-sumFirstAndLastNumber.js
@@ -3,6 +3,7 @@ function sumFirstAndLast(str) {
     let last = Number(str.pop());
     console.log(first + last);
 }
+
 sumFirstAndLast(['20', '30', '40']); // 60
 sumFirstAndLast(['10', '17', '22', '33']); // 43
 sumFirstAndLast(['11', '58', '69']); // 80
