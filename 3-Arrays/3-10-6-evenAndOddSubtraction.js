7135b51fe960cb605ace828c5cef7b6e79c8011e
Albena Kostova <38568843+malykdim@users.noreply.github.com>
2020-12-23 22:06:31 +0100

    Update 3-10-6-evenAndOddSubtraction.js

--- 3-Arrays/3-10-6-evenAndOddSubtraction.js
+++ 3-Arrays/3-10-6-evenAndOddSubtraction.js
@@ -1,13 +1,11 @@
-function evenAndOddSubtraction(arr) {
-    // parse each string to number
+function evenAndOddSubtraction(arr) { 
     for (let i = 0; i < arr.length; i++) {
         arr[i] = Number(arr[i]); 
     }
-    // create 2 variables for the sums
+   
     let evenSum = 0; 
     let oddSum = 0; 
-    // Iterate through all elements in the array with for-of loop
-    // and check if the number is odd or even
+    
     for(let num of arr) {
         if(num % 2 === 0) {
             evenSum += num;
@@ -15,9 +13,10 @@ function evenAndOddSubtraction(arr) {
             oddSum += num;
         }
     }
-    // print the difference
+    
     console.log(evenSum - oddSum);
 }
-evenAndOddSubtraction(['1', '2', '3', '4', '5', '6']); // 3
-evenAndOddSubtraction(['3', '5', '7', '9']); // -24
-evenAndOddSubtraction(['2', '4', '6', '8', '10']); // 30
+
+evenAndOddSubtraction(['1', '2', '3', '4', '5', '6']);
+evenAndOddSubtraction(['3', '5', '7', '9']); 
+evenAndOddSubtraction(['2', '4', '6', '8', '10']); 
