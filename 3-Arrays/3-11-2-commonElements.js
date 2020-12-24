98d07bca08c917afdd08744c6ef07e228f49b307
Albena Kostova <38568843+malykdim@users.noreply.github.com>
2020-12-24 16:26:30 +0100

    Update 3-11-2-commonElements.js

--- 3-Arrays/3-11-2-commonElements.js
+++ 3-Arrays/3-11-2-commonElements.js
@@ -1,4 +1,4 @@
-/* function commonElements(str1, str2) {
+function commonElements(str1, str2) {
     for (let k = 0; k < str1.length; k++) {
         for (let item of str2) {
             if (str1[k] === item) {
@@ -7,12 +7,13 @@
         }
     }
 }
-// commonElements(['Hey', 'hello', 2, 4, 'Peter', 'e'], ['Petar', 10, 'hey', 4, 'hello', '2']); // hello 4
-// commonElements(['S', 'o', 'f', 't', 'U', 'n', 'i', ' '], ['s', 'o', 'c', 'i', 'a', 'l']); // o i
-commonElements(["S", "o", "f", "t", "U", "n", "i", " "], ["s", "o", "c", "i", "a", "l"]); // 
-commonElements(["Hey", "hello", 2, 4, "Peter", "e"], ["Petar", 10, "hey", 4, "hello", "2"]); // 
-// 100/100 // mySolution
- */
+
+// 100/100
+
+commonElements(['Hey', 'hello', 2, 4, 'Peter', 'e'], ['Petar', 10, 'hey', 4, 'hello', '2']); 
+commonElements(['S', 'o', 'f', 't', 'U', 'n', 'i', ' '], ['s', 'o', 'c', 'i', 'a', 'l']); 
+commonElements(["S", "o", "f", "t", "U", "n", "i", " "], ["s", "o", "c", "i", "a", "l"]); 
+commonElements(["Hey", "hello", 2, 4, "Peter", "e"], ["Petar", 10, "hey", 4, "hello", "2"]); 
 
 /* Deni */
 /* 1 */
@@ -35,6 +36,4 @@ commonElements(["Hey", "hello", 2, 4, "Peter", "e"], ["Petar", 10, "hey", 4, "he
 //         }
 //     }
 // }
-commonElements(["S", "o", "f", "t", "U", "n", "i", " "], ["s", "o", "c", "i", "a", "l"]); // 
-commonElements(["Hey", "hello", 2, 4, "Peter", "e"], ["Petar", 10, "hey", 4, "hello", "2"]); // 
 // 100/100 // for both
