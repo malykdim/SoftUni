9051890d91d453bea3eb56fbeaa597702e1b76bf
Albena Kostova <38568843+malykdim@users.noreply.github.com>
2020-12-27 18:03:27 +0100

    Update 4-14-1-smallestOfThreeNumbers.js

--- 4-Functions/4-14-1-smallestOfThreeNumbers.js
+++ 4-Functions/4-14-1-smallestOfThreeNumbers.js
@@ -7,27 +7,12 @@ function smallestOfThreeNumbers(int1, int2, int3) {
             minNumber = numbers[i];
         } 
     }
-    console.log(minNumber);
-}
-// 100/100
-smallestOfThreeNumbers(2, 5, 3); // 2
-smallestOfThreeNumbers(600, 342, 123); // 123
-smallestOfThreeNumbers(25, 21, 4); // 4
-
-/* Deni */
-
-function smallestOfThreeNumbers(int1, int2, int3) {
-    let numbers = [int1, int2, int3];
-    let minNumber = 0;
     
-    for (i = 0; i < numbers.length; i++) {
-        if (minNumber === 0 || numbers[i] < minNumber) {
-            minNumber = numbers[i];
-        } 
-    }
     console.log(minNumber);
 }
-// 100/100
-smallestOfThreeNumbers(2, 5, 3); // 2
-smallestOfThreeNumbers(600, 342, 123); // 123
-smallestOfThreeNumbers(25, 21, 4); // 4  
+
+// 100/100 /* Deni */
+
+smallestOfThreeNumbers(2, 5, 3); 
+smallestOfThreeNumbers(600, 342, 123); 
+smallestOfThreeNumbers(25, 21, 4); 
