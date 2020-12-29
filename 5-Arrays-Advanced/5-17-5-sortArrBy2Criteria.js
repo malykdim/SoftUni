b0c42f25e67a7c16e9d2f859975236bf0d1b4d89
Albena Kostova <38568843+malykdim@users.noreply.github.com>
2020-12-29 20:41:17 +0100

    Update 5-17-5-sortArrBy2Criteria.js

--- 5-Arrays-Advanced/5-17-5-sortArrBy2Criteria.js
+++ 5-Arrays-Advanced/5-17-5-sortArrBy2Criteria.js
@@ -3,9 +3,9 @@ function sortBy2Criteria(arr) {
     for(let item of lengthAsc) {
         console.log(item);        
     }    
-} // 100/100
-// sortBy2Criteria(["alpha", "beta", "gamma"]);
-// sortBy2Criteria(["Isacc", "Theodor", "Jack", "Harrison", "George"]);
+} 
+// 100/100
+
+sortBy2Criteria(["alpha", "beta", "gamma"]);
+sortBy2Criteria(["Isacc", "Theodor", "Jack", "Harrison", "George"]);
 sortBy2Criteria(["test", "Deny", "omen", "Default"]);
-
-
