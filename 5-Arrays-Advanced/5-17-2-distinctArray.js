c5d755be67f56163d225ef96c9fa10fba2560d14
Albena Kostova <38568843+malykdim@users.noreply.github.com>
2020-12-29 20:39:29 +0100

    Update 5-17-2-distinctArray.js

--- 5-Arrays-Advanced/5-17-2-distinctArray.js
+++ 5-Arrays-Advanced/5-17-2-distinctArray.js
@@ -7,10 +7,11 @@ function distinctArray(arr) {
             index = arr.indexOf(arr[i], i + 1);
         }
     }
+    
     return arr.join(' ');
 }
+
 // Deni 100/100
-// distinctArray([1, 2, 3, 4]); // 
+
 console.log(distinctArray([7, 8, 9, 7, 2, 3, 4, 1, 2])); // 7 8 9 2 3 4 1
 console.log(distinctArray([20, 8, 12, 13, 4, 4, 8, 5])); // 20 8 12 13 4 5
-
