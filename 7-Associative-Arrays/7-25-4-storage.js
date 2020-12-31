464af8af2aa48889127ee13349af939851230fa3
Albena Kostova <38568843+malykdim@users.noreply.github.com>
2020-12-31 19:43:39 +0100

    Update 7-25-4-storage.js

--- 7-Associative-Arrays/7-25-4-storage.js
+++ 7-Associative-Arrays/7-25-4-storage.js
@@ -1,24 +1,3 @@
-/* function storage(input) {
-    let storage = new Map();
-    
-    for(let line of input) {
-        let [product, qty] = line.split(' ');
-        qty = Number(qty);
-        
-        if (storage.has(product)) {
-            let existing = storage.get(product);
-            storage.set(product, existing + qty);
-            storage.set(product, existing + qty);
-        } else {
-            storage.set(product, qty);
-        }
-    }
-    
-    for (let [product, qty] of storage) {
-        console.log(`${product} -> ${qty}`);
-    }
-} */
-
 function storage(input) {
     let storage = new Map();
     
@@ -44,3 +23,24 @@ storage([
 'olives 100',
 'coffee 40'
 ]);
+
+/* function storage(input) {
+    let storage = new Map();
+    
+    for(let line of input) {
+        let [product, qty] = line.split(' ');
+        qty = Number(qty);
+        
+        if (storage.has(product)) {
+            let existing = storage.get(product);
+            storage.set(product, existing + qty);
+            storage.set(product, existing + qty);
+        } else {
+            storage.set(product, qty);
+        }
+    }
+    
+    for (let [product, qty] of storage) {
+        console.log(`${product} -> ${qty}`);
+    }
+} */
