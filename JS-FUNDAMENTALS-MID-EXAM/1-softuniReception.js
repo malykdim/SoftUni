0d5ed6df2e94cd5f48f90c5b12f3749ecd32d1fd
Albena Kostova <38568843+malykdim@users.noreply.github.com>
2021-01-04 14:26:12 +0100

    Update 1-softuniReception.js

--- JS-FUNDAMENTALS-MID-EXAM/1-softuniReception.js
+++ JS-FUNDAMENTALS-MID-EXAM/1-softuniReception.js
@@ -12,7 +12,9 @@ function softuniReception(input) {
     }
     console.log(`Time needed: ${hour}h.`);
 }
+
 // 100/100
+
 softuniReception(['5', '6', '4', '20']); 
 softuniReception(['1', '2', '3', '45']); 
 softuniReception(['3', '2', '5', '40']); 
