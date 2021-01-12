6d820e117b6b79eb3a21d3973a2cd64884fbff1f
Albena Kostova <albena.kostov@gmail.com>
2021-01-12 16:39:41 +0100

    HTML & CSS

--- JS-ADVANCED/1-SYNTAX/lab-4-largestNumber.js
+++
@@ -1,8 +0,0 @@
-function largestNumber(num1, num2, num3) {
-    console.log(`The largest number is ${Math.max(num1, num2, num3)}.`);
-}
-
-largestNumber(5, -3, 16);
-largestNumber(-3, -5, -22.5);
-
-// 100/100
