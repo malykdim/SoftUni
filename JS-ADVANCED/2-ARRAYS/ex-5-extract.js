a197b75cb98d9efc2969e23da1ecfa41ade8abb4
Albena Kostova <38568843+malykdim@users.noreply.github.com>
2021-01-15 20:21:07 +0100

    Add files via upload

---
+++ JS-ADVANCED/2-ARRAYS/ex-5-extract.js
@@ -0,0 +1,30 @@
+function solve(arr) {
+
+    return
+}
+
+console.log(solve([
+    1,
+    3,
+    8,
+    4,
+    10,
+    12,
+    3,
+    2,
+    24
+]));
+console.log(solve([
+    1,
+    2,
+    3,
+    4
+]));
+console.log(solve([
+    20,
+    3,
+    2,
+    15,
+    6,
+    1
+]));
