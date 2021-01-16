828b501788123eda98e37064a7df7e49dc9471b1
Albena Kostova <38568843+malykdim@users.noreply.github.com>
2021-01-16 14:57:44 +0100

    Add files via upload

---
+++ JS-ADVANCED/2-ARRAYS/ex-7-sortingNums.js
@@ -0,0 +1,13 @@
+function sortingNums(arr) {
+    arr.sort((a, b) => a - b);
+    let result = [];
+    while (arr.length) {
+        result.push(arr.shift());
+        result.push(arr.pop());
+    }
+    return result.filter(num => num != undefined);
+}
+
+console.log(sortingNums([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]));
+
+// 100/100
