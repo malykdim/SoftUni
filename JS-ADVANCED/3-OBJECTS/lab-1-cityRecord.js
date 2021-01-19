2088423e49353a28c3e59d0a9518776d506be6b3
Albena Kostova <albena.kostov@gmail.com>
2021-01-19 17:13:29 +0100

    Create lab-1-cityRecord.js

---
+++ JS-ADVANCED/3-OBJECTS/lab-1-cityRecord.js
@@ -0,0 +1,14 @@
+function solve(name, population, treasury) {
+    let obj = {
+        name,
+        population,
+        treasury
+    }
+
+    return obj;
+}
+
+console.log(solve('Tortuga', 7000, 15000));
+console.log(solve('Santo Domingo', 12000, 23500));
+
+// 100/100
