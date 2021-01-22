a9729f22aab91389be07c78cf7a90cf37b6cea49
Albena Kostova <38568843+malykdim@users.noreply.github.com>
2021-01-22 15:39:48 +0100

    Add files via upload

---
+++ JS-ADVANCED/4-DOM/CollectListItems.js
@@ -0,0 +1,8 @@
+function extractText() {
+    const liElements = [...document.getElementsByTagName('li')];
+    const elementText = liElements.map(e => e.textContent);
+    
+    document.getElementById('result').value = elementText.join('\n');
+}
+
+// 100/100
