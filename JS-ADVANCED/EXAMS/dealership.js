5ce55e773572d11d840b429ffcfb06a482a77c04
Albena Kostova <38568843+malykdim@users.noreply.github.com>
2021-02-18 11:59:45 +0100

    Update dealership.js

--- JS-ADVANCED/EXAMS/dealership.js
+++ JS-ADVANCED/EXAMS/dealership.js
@@ -1,3 +1,5 @@
+const expect = require('chai').expect;
+
 let dealership = {
     newCarCost: function (oldCarModel, newCarPrice) {
 
@@ -37,5 +39,40 @@ let dealership = {
     }
 };
 
-module.exports = dealership;
-
+describe('test', function() {
+    describe('newCarCost', () => {
+        it('returns original price when model unsupported', () => {
+            expect(dealership.newCarCost('a', 1)).to.equal(1);
+        });
+        
+        it('returns discounted price', () => {
+            expect(dealership.newCarCost('Audi A4 B8', 30000)).to.equal(15000);
+        });
+    });
+    
+    describe('carEquipment', () => {
+        it('single element, single pick', () => {
+            expect(dealership.carEquipment(['a'], [0])).to.deep.equal(['a']);
+        });
+        
+        it('single element, single pick', () => {
+            expect(dealership.carEquipment(['a', 'b', 'c'], [0, 2])).to.deep.equal(['a', 'c']);
+        });
+        
+        it('single element, single pick', () => {
+            expect(dealership.carEquipment(['a', 'b', 'c'], [1])).to.deep.equal(['b']);
+        });
+    });
+    
+    describe('euroCategory', () => {
+        it('category is below threshold', () => {
+            expect(dealership.euroCategory(1)).to.equal('Your euro category is low, so there is no discount from the final price!');
+        });
+        it('category is above threshold', () => {
+            expect(dealership.euroCategory(5)).to.equal('We have added 5% discount to the final price: 14250.');
+        });
+        it('category is at threshold (edge case)', () => {
+            expect(dealership.euroCategory(4)).to.equal('We have added 5% discount to the final price: 14250.');
+        });
+    });
+});
